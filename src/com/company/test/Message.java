7529d58656369cb0aabd9e5b94a345b207d3127c
MiMo1107 <mikkel1107@gmail.com>
2016-09-12 09:27:03 +0200

    Message og flytte user

--- src/com/company/test/Message.java
+++ src/com/company/test/Message.java
@@ -1,32 +1,43 @@
 package com.company.test;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * Created by mikkel on 10-09-2016.
  */
 public class Message implements Serializable {
 
-    // The different types of message sent by the Client
-    // WHOISIN to receive the list of the users connected
-    // MESSAGE an ordinary message
-    // LOGOUT to disconnect from the Server
-    public static final int WHOISIN = 0, MESSAGE = 1, LOGOUT = 2;
+    public static final int JOIN = 0, J_OK = 1, J_ERR = 2, DATA = 3, ALVE = 4, QUIT = 5, LIST = 6;
     private int type;
     private String message;
+    private ArrayList<User> userList;
 
     // constructor
+    public Message(int type) {
+        this.type = type;
+    }
+
     public Message(int type, String message) {
         this.type = type;
         this.message = message;
     }
 
-    // getters
+    public Message(int type, ArrayList<User> userList){
+        this.userList = userList;
+    }
+
     public int getType() {
         return type;
     }
+
     public String getMessage() {
         return message;
     }
+
+    public ArrayList getUserList(){
+        return userList;
+    }
+
 }
 
