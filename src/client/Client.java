b75cc54c1fd1b763255753b38ce4835230ae8316
MiMo1107 <mikkel1107@gmail.com>
2016-09-21 10:52:54 +0200

    New test

---
+++ src/client/Client.java
@@ -0,0 +1,131 @@
+package client;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * Created by mikkel on 12-09-2016.
+ */
+public class Client {
+
+    private Socket socket;
+    private ObjectInputStream inputStream;
+    private ObjectOutputStream outputStream;
+    private String serverAddress, username;
+    private int serverPort;
+    private boolean connected;
+
+    public Client (String connectAddress, String username, int connectPort){
+        this.serverAddress = connectAddress;
+        this.username = username;
+        this.serverPort = connectPort;
+    }
+
+    public boolean start(){
+        try {
+
+            socket = new Socket(serverAddress, serverPort);
+            outputStream = new ObjectOutputStream(socket.getOutputStream());
+            inputStream  = new ObjectInputStream(socket.getInputStream());
+            outputStream.writeObject(new Message(username, Message.JOIN));
+
+        } catch (Exception e){
+            display("Unsuccessful login: " + e);
+            return false;
+        }
+
+        connected = true;
+        Thread serverListener = new Thread(() -> {
+            while(true) {
+                try {
+                    Message message = (Message) inputStream.readObject();
+                    switch(message.getType()) {
+
+                        case Message.DATA:
+                            display(message.getUsername()+ ": " + message.getMessage());
+                            break;
+                        case Message.J_ERR:
+                            display("Error Username " + username + " already in use. Try something else.");
+                            disconnect();
+                            break;
+                        case Message.J_OK:
+                            display("Successful login");
+                            break;
+                        case Message.LIST:
+                            clientGUI.listClients(message.getclientList());
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch(IOException e) {
+                    if(connected){
+                        display("Server timed out3: " + e);
+                    }
+                    break;
+                }
+                catch(ClassNotFoundException e2) {
+                }
+            }
+        }
+        );
+        serverListener.start();
+
+        Thread heartBeat = new Thread(()-> {
+            while(true) {
+                try {
+                    Thread.sleep(60000);
+                    outputStream.writeObject(new Message(username, Message.ALVE));
+                }
+                catch(IOException e) {
+                    display("Server timed out1: " + e);
+                    break;
+                } catch (InterruptedException e) {
+                    display("Server timed out2: " + e);
+                    break;
+                }
+            }
+        });
+
+        heartBeat.start();
+        return true;
+    }
+
+    public void display(String text){
+        clientGUI.writeTextToGUI(text);
+    }
+
+    public void sendMessage(Message message){
+        try {
+            outputStream.writeObject(message);
+        }
+        catch(IOException ex) {
+            display("Exception writing to server: " + ex);
+        }
+    }
+
+    public void disconnect(){
+        connected = false;
+        try {
+            if(inputStream != null){
+                inputStream.close();
+            }
+            if(outputStream != null){
+                outputStream.close();
+            }
+            if(socket != null){
+                socket.close();
+            }
+            display("Successfully disconnected");
+        }
+        catch(Exception ex){
+            display("Exception when disconnecting: " + ex);
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
---
+++ src/server/Server.java
@@ -0,0 +1,108 @@
+package server;
+
+import java.io.Serializable;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ * Created by mikkel on 12-09-2016.
+ */
+public class Server {
+
+    private static int clientId;
+    private ArrayList<ActiveClient> clientList;
+    private int serverPort;
+    private boolean running;
+    private String serverUsername;
+    private ServerGUI serverGUI;
+
+    public Server(int serverPort, ServerGUI serverGUI, String serverUsername){
+        this.serverPort = serverPort;
+        clientList = new ArrayList<>();
+        this.serverUsername = serverUsername;
+        this.serverGUI = serverGUI;
+        this.serverGUI = serverGUI;
+    }
+
+    public void start() {
+        running = true;
+        try
+        {
+            ServerSocket serverSocket = new ServerSocket(serverPort);
+            display("Server up and running");
+            while(running)
+            {
+                Socket socket = serverSocket.accept();
+
+                if(!running)
+                    break;
+
+                ActiveClient activeClientThread = new ActiveClient(socket, ++clientId, this);
+                clientList.add(activeClientThread);
+                activeClientThread.start();
+                updateActiveClientList();
+            }
+            serverSocket.close();
+            for(ActiveClient activeClient : clientList){
+                activeClient.close();
+            }
+        }
+        catch(Exception e) {
+            display("Error when closing the server and clients: " + e);
+        }
+
+    }
+
+    protected void stop() {
+        running = false;
+        try {
+            new Socket("localhost", serverPort);
+        }
+        catch(Exception e) {
+        }
+    }
+
+    public void display(String textMessage) {
+        serverGUI.writeTextToGUI(textMessage);
+    }
+
+    public void broadcast(Message message) {
+        for (int i = clientList.size(); --i >= 0; ) {
+            ActiveClient activeClient = clientList.get(i);
+            if (!activeClient.writeToThisClient(message)) {
+                clientList.remove(i);
+                display("Disconnected Client " + activeClient.getUsername() + " removed from list.");
+            }
+        }
+        if(message.getType() == Message.DATA){
+            display(message.getUsername() + " :" + message.getMessage());
+        } else if(message.getType() == Message.LIST){
+            serverGUI.listClients(message.getclientList());
+        }
+
+    }
+
+    public void removeClient(int id) {
+        for(ActiveClient activeClient : clientList){
+            if(activeClient.getId() == id) {
+                clientList.remove(activeClient.getId());
+                updateActiveClientList();
+                return;
+            }
+        }
+    }
+
+    public void updateActiveClientList(){
+        broadcast(new Message(Message.LIST, clientList));
+    }
+
+    public ArrayList<ActiveClient> getClientList() {
+        return clientList;
+    }
+
+    public String getServerUsername() {
+        return serverUsername;
+    }
+}
+
