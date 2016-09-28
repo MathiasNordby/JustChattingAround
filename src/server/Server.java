dd54d5beccef5d3c48a1e0b2716ba52a2127fd28
TanjaValhoj <Tanja.valhoj@gmail.com>
2016-09-28 22:12:18 +0200

    //Kommentarer på Server

--- src/server/Server.java
+++ src/server/Server.java
@@ -18,13 +18,14 @@ public class Server {
     public Server() {
 
     }
-
+    //Starter server
     public void start() {
         running = true;
         clientList = new ArrayList<>();
         try {
             Scanner scan = new Scanner(System.in);
 
+            //Tjekker at indtastet ServerPort er gyldig, og godkender hvis den er.
             Boolean verfiyInput = false;
             while (!verfiyInput) {
                 display("Insert server port:");
@@ -34,11 +35,13 @@ public class Server {
                 }
             }
 
+            //Imens serveren kører, ServerSocket accepteres, Portnummer fastlagt for denne server
             ServerSocket serverSocket = new ServerSocket(serverPort);
             display("Server up and running");
             while (running) {
                 Socket socket = serverSocket.accept();
 
+                //ActiveClientThread bliver tilføjet til clientList. ActiveClientThread startes, og listen opdateres
                 ActiveClient activeClientThread = new ActiveClient(socket, ++clientId, this);
                 clientList.add(activeClientThread);
                 activeClientThread.start();
@@ -46,6 +49,7 @@ public class Server {
                 updateActiveClientList();
             }
             serverSocket.close();
+            //Hvis der opstår en fejl, kører Activeclient igennem og lukker listen
             for (ActiveClient activeClient : clientList) {
                 activeClient.close();
             }
@@ -54,7 +58,7 @@ public class Server {
         }
 
     }
-
+    //Stopper serveren, gør klar så man kan oprette en ny / anden server
     protected void stop() {
         running = false;
         try {
@@ -62,11 +66,13 @@ public class Server {
         } catch (Exception e) {
         }
     }
-
+    //Printer textMessage
     public void display(String textMessage) {
         System.out.println(textMessage);
     }
 
+    //Tjekker om der er en client der ikke er aktiv, og fjerner den fra ActiveClients listen.
+    //Hvis client ikke er active: Besked om at den gældende client er disconnectet
     public void broadcast(MessageServer message) {
         for (int i = clientList.size(); --i >= 0; ) {
             ActiveClient activeClient = clientList.get(i);
@@ -75,11 +81,12 @@ public class Server {
                 display("Disconnected Client " + activeClient.getUsername() + " removed from list.");
             }
         }
+        //Hvis de to nederstående er lig hinanden, viser den usernavnet og deres message i consolen
         if (message.getType() == MessageServer.DATA) {
             display(message.getUser_name() + ": " + message.getText());
         }
     }
-
+    //Fjerner ikke aktive clients ud fra deres id.
     public void removeClient(int id) {
         for (ActiveClient activeClient : clientList) {
             if (activeClient.getId() == id) {
@@ -89,15 +96,16 @@ public class Server {
             }
         }
     }
-
+    //Laver en String med ActiveClients, med deres username
     public void updateActiveClientList() {
         String list = "LIST ";
         for (ActiveClient client : clientList) {
             list = list + client.getUsername() + " ";
         }
+        //Broadcaster List, til LIST i MessageServer
         broadcast(new MessageServer(MessageServer.LIST, list));
     }
-
+    //Tjekker at port nummer er gyldigt
     public boolean portVerify(int port) {
         if (!(port <= 65535 && port >= 0)) {
             display("Insert valid port number. Note only numbers allowed, between 0-65535.");
@@ -106,7 +114,7 @@ public class Server {
             return true;
         }
     }
-
+    // Retunerer clientList
     public ArrayList<ActiveClient> getClientList() {
         return clientList;
     }
